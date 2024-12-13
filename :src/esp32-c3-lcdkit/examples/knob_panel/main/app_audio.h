b32339387b47f9c08d4bc095ddb63dc3e42d7daa
agabdul <113071300+agabdul@users.noreply.github.com>
2024-12-13 18:42:23 -0500

    Initial commit

---
+++ :src/esp32-c3-lcdkit/examples/knob_panel/main/app_audio.h
@@ -0,0 +1,26 @@
+
+/*
+ * SPDX-FileCopyrightText: 2023 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+#pragma once
+
+typedef enum{
+    SOUND_TYPE_KNOB,
+    SOUND_TYPE_SNORE,
+    SOUND_TYPE_WASH_END_CN,
+    SOUND_TYPE_WASH_END_EN,
+    SOUND_TYPE_FACTORY,
+	SOUND_TYPE_25,
+	SOUND_TYPE_50,
+	SOUND_TYPE_75,
+	SOUND_TYPE_100
+}PDM_SOUND_TYPE;
+
+esp_err_t audio_force_quite(bool ret);
+
+esp_err_t audio_handle_info(PDM_SOUND_TYPE voice);
+
+esp_err_t audio_play_start();
